4b6d61cd0b902a5958f4c4762141eed8cf883518
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-12-04 14:36:07 +0100

    Theme Box collapse, stile

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -4,22 +4,20 @@
 		<div class="themeContainer">
 			<!-- Theme Option -->
 			<div
-				class="themeOption closed"
-				:class="!varTheme ? 'closed' : 'opened'"
+				:class="varTheme ? 'opened themeOption' : 'closed themeOption'"
 			>
-				<div>Theme Option</div>
-				<div>Light</div>
-				<div>Dark</div>
+				<div class="themeTitle">Theme Option</div>
+				<div class="themeLight">Light</div>
+				<div class="themeDark">Dark</div>
 			</div>
 
 			<!-- Icon sole -->
-			<div :class="varTheme ? 'sunOpen' : 'sunClosed'" class="sunCont">
+			<div
+				:class="varTheme ? 'sunOpen sunCont' : 'sunClosed sunCont'"
+				@click="openTheme()"
+			>
 				<div class="sun">
-					<font-awesome-icon
-						@click="openTheme()"
-						class=""
-						icon="fa-solid fa-sun"
-					/>
+					<font-awesome-icon class="" icon="fa-solid fa-sun" />
 				</div>
 			</div>
 		</div>
@@ -91,11 +89,13 @@ export default {
 
 // Sun CLass
 .sunClosed {
-	transform: translateX(-450%);
+	transform: translateX(-550%);
 }
 .sunOpen {
 	transform: translateX(0%);
 }
+
+// Container Theme
 .themeContainer {
 	position: fixed;
 	top: 30%;
@@ -103,20 +103,60 @@ export default {
 	display: flex;
 	align-items: center;
 
+	// Contenitore Theme option
 	.themeOption {
-		background-color: white;
-		padding: 10px 15px 10px 10px;
+		background-color: rgba(234, 242, 248, 0.521);
 		font-size: 22px;
 		border-radius: 0px 20px 20px 0;
 		box-shadow: 0px 10px 20px rgb(100, 99, 99);
 		transition: all 0.5s;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 4px;
+
+		// Titoo them box
+		.themeTitle {
+			padding: 10px 35px;
+		}
+
+		// Tema Chiaro
+		.themeLight {
+			margin-top: 20px;
+			margin-bottom: 20px;
+			padding: 10px 25px;
+			text-align: center;
+			width: 60%;
+			border: 1px solid rgba(128, 128, 128, 0.13);
+			border-radius: 5px;
+			background-color: rgba(155, 155, 155, 0.055);
+			&:hover {
+				background-color: rgba(155, 155, 155, 0.15);
+			}
+		}
+
+		// tema Scuro
+		.themeDark {
+			padding: 10px 25px;
+			margin-bottom: 20px;
+			text-align: center;
+			width: 60%;
+			border: 1px solid rgba(12, 3, 133, 0.384);
+			border-radius: 5px;
+			background-color: rgba(18, 20, 155, 0.541);
+			&:hover {
+				background-color: rgba(1, 14, 133, 0.555);
+			}
+		}
 	}
 
+	// Contenitore Sun
 	.sunCont {
 		background-color: white;
 		border-radius: 0px 15px 15px 0px;
 		transition: all 0.5s;
 
+		// Sun
 		.sun {
 			padding: 10px;
 			animation-name: theme;
